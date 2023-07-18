ba1358e0fb45a3f970d9a415348727bbb14daee9
othmanelbiad <othmanelbiadpro@yahoo.com>
2023-07-18 09:33:07 -0700

    b

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
-*main - Entry point
-*Descripttion: 'prints _putchar to stdout'
-*return: Always 0 (Success)
-**/
+ * main - Entry point
+ * Discription: 'Check for number if it is positive or negative'
+ * Return: always 0
+ */
 
 int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
+
 	for (ch = 0; ch < 8; ch++)
 		_putchar(str[str]);
 	_putchar('\n');
